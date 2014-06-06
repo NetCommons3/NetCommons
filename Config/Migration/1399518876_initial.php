762c8f16537d4d0543cef86a84aec84cdd55353c
topaz2 <topaz2.3333@gmail.com>
2014-06-06 11:21:17 +0900

    Fixed typo

--- Config/Migration/1399518876_initial.php
+++ Config/Migration/1399518876_initial.php
@@ -985,7 +985,7 @@ class Initial extends CakeMigration {
 					),
 					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
 				),
-				'user' => array(
+				'users' => array(
 					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
 					'username' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 					'password' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),

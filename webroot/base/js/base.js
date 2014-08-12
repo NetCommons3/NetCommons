1df3b1b7651d729676f03deda1fe0537bf2c67d3
MIYAGAWA Takako <nekoget@gmail.com>
2014-08-12 07:22:15 +0000

    javascript fix

--- webroot/base/js/base.js
+++ webroot/base/js/base.js
@@ -16,10 +16,16 @@ NetCommonsApp.controller('NetCommons.base', function($scope) {
  * @type {{mode: string, menubar: string, plugins: string, toolbar: string}}
  */
     $scope.tinymceOptions = {
-        mode : 'exact',
-        menubar: '',
+        mode: 'exact',
+        menubar: ' ',
         plugins: 'textcolor advlist autolink autoresize charmap code link ',
-        toolbar: 'undo redo  | forecolor | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link |'
+        toolbar: 'undo redo  |' +
+            ' forecolor |' +
+            ' styleselect |' +
+            ' bold italic |' +
+            ' alignleft aligncenter alignright alignjustify |' +
+            ' bullist numlist outdent indent |' +
+            ' link |'
     };
 
 /**
@@ -29,23 +35,23 @@ NetCommonsApp.controller('NetCommons.base', function($scope) {
  * @type {{id: number, hierarchy: number}[]}
  */
     $scope.roomParts = [
-        {id:1, hierarchy:2147483647},
-        {id:2, hierarchy:8000},
-        {id:3, hierarchy:7000},
-        {id:4, hierarchy:6000},
-        {id:5, hierarchy:1000}
+        {id: 1, hierarchy: 2147483647},
+        {id: 2, hierarchy: 8000},
+        {id: 3, hierarchy: 7000},
+        {id: 4, hierarchy: 6000},
+        {id: 5, hierarchy: 1000}
     ];
 
 /**
  * idからヒエラルキーを取得する。
  * $scope.roomParts から。
  *
- * @param _id
- * @returns {*}
+ * @param {number} _id
+ * @type {id: number, hierarchy: number}
  */
     $scope.getHierarchy = function(_id) {
-        return $.grep($scope.roomParts, function(item){
-            return item.id == _id
+        return $.grep($scope.roomParts, function(item) {
+            return item.id == _id;
         })[0];
-    }
+    };
 });

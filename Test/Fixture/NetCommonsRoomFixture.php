77e50f9a9da5a3561351f5abc65589a2690a1ec8
MIYAGAWA Takako <nekoget@gmail.com>
2014-08-12 06:02:58 +0000

    add Room Model.

---
+++ Test/Fixture/NetCommonsRoomFixture.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * FrameFixture
+ *
+ * @author   Takako Miyagawa <nekoget@gmail.com>
+ * @link     http://www.netcommons.org NetCommons Project
+ * @license  http://www.netcommons.org/license.txt NetCommons License
+ */
+
+/**
+ * Summary for FrameFixture
+ */
+class NetCommonsRoomFixture extends CakeTestFixture {
+
+/**
+ * table
+ *
+ * @var string
+ */
+	public $table = 'rooms';
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'group_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'space_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'need_approval' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'top_page_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'created_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'group_id' => 1,
+			'space_id' => 1,
+			'need_approval' => 1,
+			'top_page_id' => 1,
+		),
+		array(
+			'id' => 2,
+			'group_id' => 2,
+			'space_id' => 2,
+			'need_approval' => 0,
+			'top_page_id' => 1,
+		),
+	);
+
+}

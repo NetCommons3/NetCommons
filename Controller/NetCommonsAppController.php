fdb7644f527815dc8d37e83b480d35520093e036
s-nakajima <nakajimashouhei@gmail.com>
2014-11-20 23:24:36 +0900

    レビュー後の修正

--- Controller/NetCommonsAppController.php
+++ Controller/NetCommonsAppController.php
@@ -2,7 +2,7 @@
 /**
  * NetCommonsApp Controller
  *
- * @author Takako Miyagawa <nekoget@gmail.com>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
  * @copyright Copyright 2014, NetCommons Project
@@ -13,8 +13,33 @@ App::uses('AppController', 'Controller');
 /**
  * NetCommonsApp Controller
  *
- * @author Takako Miyagawa <nekoget@gmail.com>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\NetCommons\Controller
  */
 class NetCommonsAppController extends AppController {
+
+/**
+ * json render
+ *
+ * @param array $results results data
+ * @param string $name message
+ * @param int $status status code
+ * @return void
+ */
+	public function renderJson($results, $name = 'OK', $status = 200) {
+		$this->viewClass = 'Json';
+		$this->layout = false;
+		$this->response->statusCode($status);
+		$result = array(
+			'code' => $status,
+			'name' => $name,
+			'url' => $this->url,
+			'results' => $results,
+		);
+		$this->set(compact('result'));
+		$this->set('_serialize', 'result');
+
+		$this->render(false);
+	}
+
 }

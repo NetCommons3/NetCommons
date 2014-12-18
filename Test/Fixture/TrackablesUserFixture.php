4c1eec48c882af955475b149041108199326ed73
kteraguchi <kteraguchi@commonsnet.org>
2014-12-18 17:57:23 +0900

    Fix error of phpcs

--- Test/Fixture/TrackablesUserFixture.php
+++ Test/Fixture/TrackablesUserFixture.php
@@ -15,7 +15,7 @@ class TrackablesUserFixture extends CakeTestFixture {
 
 	public $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'trackable_id' =>  array('type' => 'integer', 'null' => false, 'default' => null),
+		'trackable_id' => array('type' => 'integer', 'null' => false, 'default' => null),
 		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)

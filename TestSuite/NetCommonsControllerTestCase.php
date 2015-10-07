241affbbc0e6510261db972fda825e394d7db4c9
s-nakajima <nakajimashouhei@gmail.com>
2015-10-07 19:05:50 +0900

    テスト見直しに関する修正

--- TestSuite/NetCommonsControllerTestCase.php
+++ TestSuite/NetCommonsControllerTestCase.php
@@ -329,4 +329,80 @@ class NetCommonsControllerTestCase extends ControllerTestCase {
 		return $result;
 	}
 
+/**
+ * viewアクションのテスト
+ *
+ * @param array $urlOptions URLオプション
+ * @param array $assert テストの期待値
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @return array テスト結果
+ */
+	protected function _testGetAction($urlOptions, $assert, $exception = null, $return = 'view') {
+		//テスト実施
+		$url = Hash::merge(array(
+			'plugin' => $this->plugin,
+			'controller' => $this->_controller,
+		), $urlOptions);
+		$result = $this->_testNcAction($url, array('method' => 'get'), $exception, $return);
+
+		if (! $exception && $assert) {
+			$this->asserts(array($assert), $result);
+		}
+
+		return $result;
+	}
+
+/**
+ * addアクションのPOSTテスト
+ *
+ * @param array $data POSTデータ
+ * @param array $urlOptions URLオプション
+ * @param string|null $exception Exception
+ * @param string $return testActionの実行後の結果
+ * @return array テスト結果
+ */
+	protected function _testPostAction($method, $data, $urlOptions, $exception = null, $return = 'view') {
+		//テスト実施
+		$url = Hash::merge(array(
+			'plugin' => $this->plugin,
+			'controller' => $this->_controller,
+		), $urlOptions);
+		$result = $this->_testNcAction($url, array('method' => $method, 'data' => $data), $exception, $return);
+
+		return $result;
+	}
+
+/**
+ * addアクションのValidateionErrorテスト
+ *
+ * @param array $data POSTデータ
+ * @param string $role ロール
+ * @param array $urlOptions URLオプション
+ * @param string|null $validationError ValidationError
+ * @return array テスト結果
+ */
+	protected function _testActionOnValidationError($method, $data, $urlOptions, $validationError = null) {
+		$data = Hash::remove($data, $validationError['field']);
+		$data = Hash::insert($data, $validationError['field'], $validationError['value']);
+
+		//テスト実施
+		$url = Hash::merge(array(
+			'plugin' => $this->plugin,
+			'controller' => $this->_controller,
+		), $urlOptions);
+		$result = $this->_testNcAction($url, array('method' => $method, 'data' => $data));
+
+		//バリデーションエラー
+		$asserts = array(
+			array('method' => 'assertNotEmpty', 'value' => $this->controller->validationErrors),
+			array('method' => 'assertTextContains', 'expected' => $validationError['message']),
+		);
+
+		//チェック
+		$this->asserts($asserts, $result);
+
+		return $result;
+	}
+
 }

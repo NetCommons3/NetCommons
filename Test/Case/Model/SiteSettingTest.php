4826fa4066da69018f7f1b507762feab6a3173bf
RyujiAMANO <ryuji@ryus.co.jp>
2015-10-09 16:57:22 +0900

    NetCommons3に残ってたテストのマージ

--- Test/Case/Model/SiteSettingTest.php
+++ Test/Case/Model/SiteSettingTest.php
@@ -48,6 +48,26 @@ class SiteSettingTest extends CakeTestCase {
 	}
 
 /**
+ * getSiteTheme サイト用テーマ名の取得
+ *
+ * @return void
+ */
+	public function testGetSiteTheme() {
+		//データがある場合
+		$ck = $this->SiteSetting->getSiteTheme();
+		$this->assertTextEquals('UnitTestTheme', $ck);
+		//該当のレコード削除  //データが無い場合を作る
+		$ck = $this->SiteSetting->find('first', array(
+			'conditions' => array('SiteSetting.key' => 'theme'),
+		));
+		$ck1 = $this->SiteSetting->delete($ck['SiteSetting']['id']);
+		$this->assertTrue($ck1);
+		//問い合わせた結果データは無いのでnullが戻る
+		$ck = $this->SiteSetting->getSiteTheme();
+		$this->assertEquals(null, $ck);
+	}
+
+	/**
  * test getTimezone
  *
  * @return void

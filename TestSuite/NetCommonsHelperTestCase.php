626b8865aa4e9db253c54d4b8a182cc4c6f8df3c
s-nakajima <nakajimashouhei@gmail.com>
2016-01-27 00:15:25 +0900

    Helper用のTestSuiteを作成

---
+++ TestSuite/NetCommonsHelperTestCase.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * NetCommonsHelperTestCase class
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('Controller', 'Controller');
+App::uses('CakeRequest', 'Network');
+App::uses('CakeResponse', 'Network');
+App::uses('View', 'View');
+App::uses('NetCommonsCakeTestCase', 'NetCommons.TestSuite');
+App::uses('CurrentControlPanel', 'NetCommons.Utility');
+
+/**
+ * NetCommonsHelperTestCase class
+ *
+ * @package NetCommons\NetCommons\TestSuite
+ * @codeCoverageIgnore
+ */
+class NetCommonsHelperTestCase extends NetCommonsCakeTestCase {
+
+/**
+ * Plugin name
+ *
+ * @var array
+ */
+	public $plugin = null;
+
+/**
+ * Helper name
+ *
+ * @var string
+ */
+	protected $_helperName = null;
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		(new CurrentControlPanel())->setLanguage();
+
+		if ($this->_helperName) {
+			$this->loadHelper($this->_helperName);
+		}
+ 	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		Current::$current = array();
+		unset($this->_Controller);
+		parent::tearDown();
+	}
+
+/**
+ * indexのテスト
+ *
+ * @return void
+ */
+	public function loadHelper($helper, $viewVars = array(), $reqestData = array()) {
+		list($plugin, $helper) = pluginSplit($helper);
+
+		$helperClass = $helper . 'Helper';
+		App::uses($helperClass, Inflector::camelize($this->plugin) . '.View/Helper');
+        $CakeRequest = new CakeRequest();
+        $CakeResponse = new CakeResponse();
+		$Controller = new Controller($CakeRequest, $CakeResponse);
+		$Controller->set($viewVars);
+		$Controller->request->data = $reqestData;
+
+		$View = new View($Controller);
+		$this->$helper = new $helperClass($View);
+	}
+
+}

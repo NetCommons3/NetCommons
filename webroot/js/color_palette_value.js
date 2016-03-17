c89893e7fa0af5a265a30a4016f35418e6daf605
RikaFujiwara <rika.fujiwara@allcreator.net>
2016-03-17 03:41:55 +0000

    カラーパレットの色パネル定義体を外出しに変更
    他のDirevtiveやmoduleからも参照可能になります

---
+++ webroot/js/color_palette_value.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview color picker color palette value define Javascript
+ * @author info@allcreator.net (Allcreator Co.)
+ */
+NetCommonsApp.constant('colorPaletteBaseColors', [
+  '#f79e61', '#f25a62', '#50add6', '#63c97b', '#7c4f6c',
+  '#9c9b7f', '#4d5361', '#000000', '#ffffff', '#ff0000'
+]);
+NetCommonsApp.constant('colorPaletteDefaultColors', [
+  '#f4d7c3', '#f4ced3', '#c0e4ed', '#d1edd5', '#e3d1dc',
+  '#e0e4cd', '#d4dbe8', '#898989', '#f4f4f4', '#0000ff',
+  '#efb791', '#ee9da3', '#95cbdb', '#a3d9ad', '#b8a0b1',
+  '#c0c2a9', '#9ca2ae', '#5e5e5e', '#d9d9d9', '#00ff00',
+  '#ea9760', '#e86c74', '#69b1c9', '#75c686', '#906f86',
+  '#9fa086', '#636974', '#323232', '#bebebe', '#ff00ff',
+  '#e5772e', '#e23b44', '#3e98b7', '#47b25e', '#683e5b',
+  '#7f7e62', '#2b303a', '#070707', '#a3a3a3', '#ffff00'
+]);

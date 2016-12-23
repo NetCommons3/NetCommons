20067c1123dbe3d42d2bf2683f5585d4a5512017
s-nakajima <nakajimashouhei@gmail.com>
2016-12-23 14:44:57 +0900

    debug

--- Test/Case/AllNetCommonsTest.php
+++ Test/Case/AllNetCommonsTest.php
@@ -28,7 +28,6 @@ class AllNetCommonsTest extends NetCommonsTestSuite {
 	public static function suite() {
 		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
 		$suite = new NetCommonsTestSuite(sprintf('All %s Plugin tests', $plugin));
-		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
 
         $Folder = new Folder(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
         $files = $Folder->tree(null, true, 'files');
@@ -41,6 +40,7 @@ class AllNetCommonsTest extends NetCommonsTestSuite {
             }
         }
 
+		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
 		return $suite;
 	}
 }

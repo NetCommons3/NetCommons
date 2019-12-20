5356f9344b202b6abbaa156325aa2184d22c007c
watura <mail@wtr.io>
2019-12-20 13:46:59 +0900

    feat(CDN): Add NetCommonsCDNCache
    - clear cache

---
+++ Utility/NetCommonsCDNCache.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * NetCommons用CDNキャッシュ Utility
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+/**
+ * NetCommons用CDNキャッシュ Utility
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\NetCommons\Utility
+ */
+class NetCommonsCDNCache
+{
+	public function clear()
+	{
+		$data = array(
+			"Site" => array(
+				"Domain" => Configure::read('App.fullBaseUrl')
+			)
+		);
+
+		$curl = curl_init();
+		$accessToken = Configure::read('CDN.accessToken');
+		$accessTokenSecret = Configure::read('CDN.accessTokenSecret');
+
+		curl_setopt($curl, CURLOPT_URL, Configure::read('CDN.apiUrl') . 'deleteallcache');
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_USERPWD, "$accessToken:$accessTokenSecret");
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+		curl_exec($curl);
+		curl_close($curl);
+	}
+}
